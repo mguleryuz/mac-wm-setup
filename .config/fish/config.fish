8c9ce76d0425fe266b38a87b9dfd003d1f04b4e0
mguleryuz <mguleryuz3@gmail.com>
2024-09-01 10:46:56 -0500

    no gap on windows, neofetch only runs on iterm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    # Check if running in iTerm2
+    if test "$TERM_PROGRAM" = "iTerm.app"
+        neofetch
+    end
 end
 
 neofetch
